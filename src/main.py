a83ae0877e4f3507bba59e11d14f0df8ad6a6e91
Ana Cecília Seixas <anaceciliacs1108@gmail.com>
2025-07-28 00:45:53 -0300

    Adiciona testes automatizados e ajustes no workflow de CI

--- src/main.py
+++ src/main.py
@@ -1,32 +1,10 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Bem-vinda à Visualiza API!"}
-
-@app.get("/health")
-def check_health():
-    return {"status": "ok"}
-
 import os
 import httpx
 from dotenv import load_dotenv
 
-load_dotenv()
-
+load_dotenv()  # Carrega variáveis do .env
 NASA_API_KEY = os.getenv("NASA_API_KEY")
-
-@app.get("/apod")
-async def get_apod():
-    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        data = response.json()
-    return data
-
-    from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -40,4 +18,14 @@ def read_root():
         "message": "👋 Seja bem-vindo(a)! Esta é a API oficial do desafio Visualiza 🚀"
     }
 
+@app.get("/health")
+def check_health():
+    return {"status": "ok"}
 
+@app.get("/apod")
+async def get_apod():
+    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        data = response.json()
+    return data
